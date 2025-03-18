32205283762458179cfb044bd39caf26ebfbb19e
SP4CEVI <95271930+SP4CEVI@users.noreply.github.com>
2025-03-18 23:05:50 +0300

    Update modalWindow.js

--- src/components/modalWindow.js
+++ src/components/modalWindow.js
@@ -1,5 +1,14 @@
-export const openPopup = (popupElement) => {
+import { updateButtonStatus } from "./validation";
+
+export const openPopup = (popupElement, config) => {
   popupElement.classList.add("popup_is-opened");
+
+  const inputList = Array.from(popupElement.querySelectorAll("input"));
+  
+  if (inputList.length > 0) {
+    const button = popupElement.querySelector(".popup__button");
+    updateButtonStatus(inputList, button, config);
+  }
 
   document.addEventListener('keydown', handlekeyEscape);
 }
